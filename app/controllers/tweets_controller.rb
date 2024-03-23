9479cf7421863d47be5e95d493a18c8287bdbdbe
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2024-03-24 00:25:03 +0530

    Perform tweet CRUD

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,49 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tweet = current_user.tweets.new(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet, notice: 'Tweet was successfully created.'
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to @tweet, notice: 'Tweet was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_url, notice: 'Tweet was successfully destroyed.'
+  end
+
+  private
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+end
