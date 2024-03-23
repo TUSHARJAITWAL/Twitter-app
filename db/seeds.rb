b8fb5700d989642df14db8fb4b0e51cfde6ed4cf
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2024-03-24 01:01:54 +0530

    Create a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+Tweet.destroy_all
+
+user = User.create!(
+  email: 'user@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+tweet_content = [
+  "Exploring the world, one code at a time.",
+  "Just another day solving bugs and solvable pieces is an art.",
+  "Committing my life to git. that problem is unmatched",
+  "In search of the perfect code and that problem is unmatched",
+  "Breaking down complex problems into solvable pieces is an art.",
+  "The joy of finally solving that problem is unmatched.",
+  "Learning never stops in the world of programming.",
+  "Code, coffee, repeat. In the world of programming.",
+]
+
+dummy_image_urls = [
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/08/image-url-3.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/08/image-url-2-768x512.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/09/image-url-15-768x513.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2018/11/image-copyrighting-10.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2018/11/image-copyrighting-12-768x512.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2018/11/image-copyrighting-13-768x431.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/02/psd-image-file-13.jpg",
+  "https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/02/psd-image-file-12.jpg"
+]
+
+8.times do |i|
+  tweet = user.tweets.create!(
+    content: tweet_content.sample
+  )
+  tweet.image.attach(io: URI.open(dummy_image_urls.sample), filename: "dummy_image_#{i}.jpg")
+end
